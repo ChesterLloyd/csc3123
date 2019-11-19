3f700f9f5d3b6bd6a96c7a11eb87c12afd3613d9
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-19 19:46:12 +0000

    Code to add note in hack file.

--- devel/hack.php
+++ devel/hack.php
@@ -2,5 +2,15 @@
 /**
  * You can put arbitrary code in here and run it
  */
+    $prs = R::dispense('note');
+    $prs->name = "Lecture 1";
+    $prs->course = "Computer Science";
+    $prs->module = "CSC3123";
+    $prs->privacy = 1;
+    $prs->comment = "Comment";
+    $prs->file = "";
+    $id = R::store($prs);
+
+
     \Support\Context::getinstance()->local()->message(\Framework\Local::MESSAGE, 'Done');
 ?>
