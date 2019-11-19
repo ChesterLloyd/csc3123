44da5aed55431133b3fb9fc515cc40d9f77c31d4
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-19 22:11:56 +0000

    Added wipe beans at start of hack file.

--- devel/hack.php
+++ devel/hack.php
@@ -3,32 +3,37 @@
  * You can put arbitrary code in here and run it
  */
 
-    // // Make a test file
-    // $file = R::dispense('file');
-    // $file->name = "Lecture 1";
-    // $file->type = "PDF";
-    // $file->size = "2.1MB";
-    // $fid = R::store($file);
-    //
-    // // Make a test note
-    // $note = R::dispense('note');
-    // $note->name = "Lecture 1";
-    // $note->course = "Computer Science";
-    // $note->module = "CSC3123";
-    // $note->privacy = 1;
-    // $note->comment = "Comment";
-    // $note->xownFile[] = $file;     // Attach this file to the note (cascade delete)
-    // $nid = R::store($note);
+    // Remove all current beans
+    R::wipe('file');
+    R::wipe('note');
+    R::wipe('review');
 
-    // Make a sample review for logged in user to "Lecture 1" note 5
-    $review = R::dispense('note');
+    // Make a test file
+    $file = R::dispense('file');
+    $file->name = "Lecture 1";
+    $file->type = "PDF";
+    $file->size = "2.1MB";
+    $fid = R::store($file);
+
+    // Make a test note
+    $note = R::dispense('note');
+    $note->name = "Lecture 1";
+    $note->course = "Computer Science";
+    $note->module = "CSC3123";
+    $note->privacy = 1;
+    $note->comment = "Comment";
+    $note->xownFile[] = $file;     // Attach this file to the note (cascade delete)
+    $nid = R::store($note);
+
+    // Make a sample review for logged in user to "Lecture 1" note
+    $review = R::dispense('review');
     $review->favourite = 1;
     $review->rating = 4;
     $review->comment = "Very good notes";
     $rid = R::store($review);
 
     // Give review to note
-    $note = R::load('note', 5);
+    // $note = R::load('note', 5);
     $note->xownReview[] = $review;      // Attach this review to the note
     R::store($note);
 
