6364088ef49641a83cc014b9082759707301827e
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-19 20:05:34 +0000

    Code to associate notes with files in hack file.

--- devel/hack.php
+++ devel/hack.php
@@ -2,14 +2,28 @@
 /**
  * You can put arbitrary code in here and run it
  */
-    $prs = R::dispense('note');
-    $prs->name = "Lecture 1";
-    $prs->course = "Computer Science";
-    $prs->module = "CSC3123";
-    $prs->privacy = 1;
-    $prs->comment = "Comment";
-    $prs->file = "";
-    $id = R::store($prs);
+
+    // Make a test note
+    $note = R::dispense('note');
+    $note->name = "Lecture 1";
+    $note->course = "Computer Science";
+    $note->module = "CSC3123";
+    $note->privacy = 1;
+    $note->comment = "Comment";
+    $note->file = "";
+    $nid = R::store($note);
+
+    // Make a test file
+    $file = R::dispense('file');
+    $file->name = "Lecture 1";
+    $file->type = "PDF";
+    $file->size = "2.1MB"
+    $bid = R::store($file);
+
+    // Attach this file to the note (cascade delete)
+    $book = R::load('note', $bid));
+    $note->xcontainsFile[] = $file;
+    R::store($note);
 
 
     \Support\Context::getinstance()->local()->message(\Framework\Local::MESSAGE, 'Done');
