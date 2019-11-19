4e222ba7fdb123af4bb3483a7ab6c7d35b99cbbb
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-19 20:17:16 +0000

    Rearranged adding file to note in hack file.

--- devel/hack.php
+++ devel/hack.php
@@ -3,16 +3,6 @@
  * You can put arbitrary code in here and run it
  */
 
-    // Make a test note
-    $note = R::dispense('note');
-    $note->name = "Lecture 1";
-    $note->course = "Computer Science";
-    $note->module = "CSC3123";
-    $note->privacy = 1;
-    $note->comment = "Comment";
-    $note->file = "";
-    $nid = R::store($note);
-
     // Make a test file
     $file = R::dispense('file');
     $file->name = "Lecture 1";
@@ -20,10 +10,15 @@
     $file->size = "2.1MB";
     $fid = R::store($file);
 
-    // Attach this file to the note (cascade delete)
-    $file = R::load('file', $fid);
-    $note->xcontainsFile[] = $file;
-    R::store($note);
+    // Make a test note
+    $note = R::dispense('note');
+    $note->name = "Lecture 1";
+    $note->course = "Computer Science";
+    $note->module = "CSC3123";
+    $note->privacy = 1;
+    $note->comment = "Comment";
+    $note->xcontainsFile[] = $file;     // Attach this file to the note (cascade delete)
+    $nid = R::store($note);
 
 
     \Support\Context::getinstance()->local()->message(\Framework\Local::MESSAGE, 'Done');
