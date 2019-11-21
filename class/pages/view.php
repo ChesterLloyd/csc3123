a05b26dc0bee62bb03a620b009cc6fda0d4f3f8e
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-21 12:03:19 +0000

    Added content to view page.

--- class/pages/view.php
+++ class/pages/view.php
@@ -20,6 +20,44 @@
  */
         public function handle(Context $context)
         {
+
+            // Get note ID from query string
+            $noteid = filter_var($_GET['note'], FILTER_VALIDATE_INT);
+
+            // Put current note bean in context
+            $note = R::load('note', $noteid);
+            $context->local()->addval('note', $note);
+
+            // Put user bean in context
+            $user = $context->user();
+            $context->local()->addval('user', $user);
+
+            // Put file beans for current note in context
+            $sql = 'SELECT F.* FROM File F
+                JOIN note N ON F.note_id = N.id
+                WHERE N.id = '.$noteid;
+            $rows = R::getAll($sql);
+            $files = R::convertToBeans('file', $rows);
+            $context->local()->addval('files', $files);
+
+            // Get rating of the notes
+            $sql = 'SELECT (SUM(R.rating) / COUNT(R.rating)) AS rating
+                FROM note N
+                JOIN review R ON N.id = R.note_id
+                GROUP BY N.id
+                WHERE N.id = '.$noteid;
+            $rating = R::getCell($sql);
+            $rating = round($rating);
+            $context->local()->addval('rating', $rating);
+
+            // Get number of favourites for the note
+            $sql = 'SELECT COUNT(R.favourite) AS favourite
+                FROM review R
+                JOIN note N ON R.note_id = N.id
+                WHERE N.id = '.$noteid.' AND R.favourite = 1';
+            $favourites = R::getCell($sql);
+            $context->local()->addval('favourites', $favourites);
+
             return '@content/view.twig';
         }
     }
