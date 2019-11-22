50137120668ca1fab5807398cf6b1756acbcbba4
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-22 19:57:51 +0000

    Added content to course page.

--- class/pages/course.php
+++ class/pages/course.php
@@ -20,6 +20,52 @@
  */
         public function handle(Context $context)
         {
+            $course = filter_var($_GET['title'], FILTER_SANITIZE_STRING);
+            $context->local()->addval('course', $course);
+            // $notes = R::find('note', 'course=? AND privacy=?', [$course, 1]);
+            // $context->local()->addval('anotes', $notes);
+
+
+            $sql = 'SELECT N.* FROM note N
+                WHERE N.course = '.$course.' AND N.privacy = 1';
+            $rows = R::getAll($sql);
+            $anotes = R::convertToBeans('note', $rows);
+            $context->local()->addval('anotes', $anotes);
+
+            // Get file icons (first file type in set is the icon)
+            $sql = 'SELECT F.* FROM note
+                JOIN file F ON N.id = F.note_id
+                WHERE N.course = '.$course.' AND N.privacy = 1
+                ORDER BY N.upload ASC, F.id ASC';
+            $rows = R::getAll($sql);
+            $afiles = R::convertToBeans('file', $rows);
+            $context->local()->addval('afiles', $afiles);
+
+
+            // Get 4 top rated notes
+            // Get notes as beans
+            $sql = 'SELECT N.* FROM note N
+                JOIN review R ON N.id = R.note_id
+                WHERE N.course = '.$course.' AND N.privacy = 1
+                GROUP BY N.id
+                ORDER BY (SUM(R.rating) / COUNT(R.rating)) DESC,
+                N.upload DESC LIMIT 4';
+            $rows = R::getAll($sql);
+            $tnotes = R::convertToBeans('note', $rows);
+            $context->local()->addval('tnotes', $tnotes);
+
+            // Get file icons (first file type in set is the icon)
+            $sql = 'SELECT F.* FROM note N
+                JOIN review R ON N.id = R.note_id
+                JOIN file F ON N.id = F.note_id
+                WHERE N.course = '.$course.' AND N.privacy = 1
+                GROUP BY N.id
+                ORDER BY (SUM(R.rating) / COUNT(R.rating)) DESC,
+                N.upload DESC, F.id ASC LIMIT 4';
+            $rows = R::getAll($sql);
+            $tfiles = R::convertToBeans('file', $rows);
+            $context->local()->addval('tfiles', $tfiles);
+
             return '@content/course.twig';
         }
     }
