9eb41f31d95bd7058c1ac6181444cb75adeccd0e
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-23 21:09:35 +0000

    Added download page.

---
+++ class/pages/download.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * A class that contains code to handle any requests for  /download/
+ */
+     namespace Pages;
+
+     use \Support\Context as Context;
+     use \Config\Config as Config;
+/**
+ * Support /download/
+ */
+    class Download extends \Framework\Siteaction
+    {
+/**
+ * Handle download operations
+ *
+ * @param object	$context	The context object for the site
+ *
+ * @return string	A template name
+ */
+        public function handle(Context $context)
+        {
+            return '@content/download.twig';
+        }
+    }
+?>
