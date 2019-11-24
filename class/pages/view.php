0414957f090cc00e7f3bc6a76504f0994c99a78a
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-24 14:35:32 +0000

    Used access control in view.

--- class/pages/view.php
+++ class/pages/view.php
@@ -21,37 +21,37 @@
  */
         public function handle(Context $context)
         {
-
             // Get note ID from query string
-            $noteid = filter_var($_GET['note'], FILTER_SANITIZE_STRING);
+            $nid = filter_var($_GET['note'], FILTER_SANITIZE_STRING);
+
+            $files = R::find('upload', 'note_id = ?', [$nid]);
+            foreach ($files as $file)
+            {
+                if (!$file->canaccess($context->user()))
+                { # current user cannot access the file, should not access notes
+                    throw new \Framework\Exception\Forbidden('No access');
+                }
+            }
+            $context->local()->addval('files', $files);
 
             // Put current note bean in context
-            $note = $context->load('note', $noteid);
+            $note = $context->load('note', $nid);
             $context->local()->addval('note', $note);
 
-            // Put user bean in context
+            // Put current user bean in context
             $user = $context->user();
-            $userid = $user->id;
+            $uid = $user->id;
             $context->local()->addval('user', $user);
 
-            // Put author's bean in context
-            // $author = R::load('user', $note->user_id);
+            // Put author bean in context
             $author = R::findOne('user', 'id=?', [$note->user_id]);
             $context->local()->addval('author', $author);
-
-            // Put file beans for current note in context
-            $sql = 'SELECT F.* FROM file F
-                JOIN note N ON F.note_id = N.id
-                WHERE N.id = '.$noteid;
-            $rows = R::getAll($sql);
-            $files = R::convertToBeans('file', $rows);
-            $context->local()->addval('files', $files);
 
             // Get rating of the notes
             $sql = 'SELECT (SUM(R.rating) / COUNT(R.rating)) AS rating
                 FROM note N
                 JOIN review R ON N.id = R.note_id
-                WHERE N.id = '.$noteid.' GROUP BY N.id';
+                WHERE N.id = '.$nid.' GROUP BY N.id';
             $rating = R::getCell($sql);
             $rating = round($rating);
             $context->local()->addval('rating', $rating);
@@ -60,7 +60,7 @@
             $sql = 'SELECT COUNT(R.favourite) AS favourite
                 FROM review R
                 JOIN note N ON R.note_id = N.id
-                WHERE N.id = '.$noteid.' AND R.favourite = 1';
+                WHERE N.id = '.$nid.' AND R.favourite = 1';
             $favourites = R::getCell($sql);
             $context->local()->addval('favourites', $favourites);
 
@@ -68,7 +68,7 @@
             $sql = 'SELECT R.favourite
                 FROM review R
                 JOIN note N ON R.note_id = N.id
-                WHERE R.user_id = '.$userid.' AND N.id = '.$noteid;
+                WHERE R.user_id = '.$uid.' AND N.id = '.$nid;
             $favourite = R::getCell($sql);
             $context->local()->addval('favourite', $favourite);
 
@@ -76,7 +76,7 @@
             $sql = 'SELECT COUNT(R.id)
                 FROM review R
                 JOIN note N ON R.note_id = N.id
-                WHERE R.user_id = '.$userid.' AND N.id = '.$noteid;
+                WHERE R.user_id = '.$uid.' AND N.id = '.$nid;
             $reviewed = R::getCell($sql);
             $context->local()->addval('reviewed', $reviewed);
 
