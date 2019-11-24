b1c91b5c90127a5c7a651752564820dbe9e7e177
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-24 22:24:48 +0000

    Added content to modify.

--- class/pages/modify.php
+++ class/pages/modify.php
@@ -20,6 +20,82 @@
  */
         public function handle(Context $context)
         {
+            // $fd = $context->formdata();
+            // if ($fd->hasfile('uploads'))
+            // {
+            //     // Make a note with the form data
+            //     $note = \R::dispense('note');
+            //     $note->name = $fd->post('name', 'No name');
+            //     $note->course = $fd->post('course', 'Unknown Course');
+            //     $note->module = $fd->post('module', 'Unknown Module');
+            //     $note->privacy = $fd->post('privacy', '0');
+            //     $note->downloads = 0;
+            //     $note->upload = $context->utcnow();
+            //     $note->description = $fd->post('description', 'No description');
+            //     $note->comment = $fd->post('comment', 'No comments.');
+            //
+            //     if (Config::UPUBLIC && Config::UPRIVATE)
+            //     { # need to check the flag could be either private or public
+            //         foreach ($fd->posta('public') as $ix => $public)
+            //         {
+            //             $upl = \R::dispense('upload');
+            //             $note->xownFile[] = $upl;
+            //             $save = $upl->savefile($context, $fd->filedata('uploads', $ix), $public, $context->user(), $ix);
+            //         }
+            //     }
+            //     else
+            //     {
+            //         foreach(new \Framework\FAIterator('uploads') as $ix => $fa)
+            //         { # we only support private or public in this case so there is no flag
+            //             $upl = \R::dispense('upload');
+            //             $note->xownFile[] = $upl;
+            //             $save = $upl->savefile($context, $fa, Config::UPUBLIC, $context->user(), $ix);
+            //         }
+            //     }
+            //     if ($save)
+            //     {
+            //         // The file(s) uploaded OK, store the note
+            //         $nid = \R::store($note);
+            //
+            //         // Then assign it to the author (logged in user)
+            //         $uid = $context->user()->id;
+            //         $user = \R::load('user', $uid);
+            //         $user->xownNote[] = $note;
+            //         \R::store($user);
+            //
+            //         // Display a success message with a link to see thier notes
+            //         $context->local()->message(Local::MESSAGE, 'Your notes have been uploaded. View them <a href=view?note=' . $nid . '>here</a>.');
+            //     }
+            // }
+
+
+            // Get existing note to page
+
+            // Get note ID from query string
+            $nid = filter_var($_GET['note'], FILTER_SANITIZE_STRING);
+
+            $files = R::find('upload', 'note_id = ?', [$nid]);
+            foreach ($files as $file)
+            {
+                if (!$file->canaccess($context->user()))
+                { # current user cannot access the file, should not access notes
+                    throw new \Framework\Exception\Forbidden('No access');
+                }
+            }
+            $context->local()->addval('files', $files);
+
+            // Put current note bean in context
+            $note = $context->load('note', $nid);
+            $context->local()->addval('note', $note);
+
+            // Put current user bean in context
+            $user = $context->user();
+            $uid = $user->id;
+            $context->local()->addval('user', $user);
+
+            // Put author bean in context (could be an admin changing this note)
+            $author = R::findOne('user', 'id = ?', [$note->user_id]);
+            $context->local()->addval('author', $author);
             return '@content/modify.twig';
         }
     }
