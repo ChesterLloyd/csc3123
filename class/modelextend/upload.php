2da3842f408223967b95e23e73e52d64e6ade3f0
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-24 01:25:12 +0000

    Fixed bug with context variable.

--- class/modelextend/upload.php
+++ class/modelextend/upload.php
@@ -44,9 +44,9 @@
             // Get current file data
             $da = $context->formdata()->filedata('uploads', $index);
             // Assign file an icon based on its type
-            $this->bean->icon = context()->getFileIcon($da['type']);
+            $this->bean->icon = $context->getFileIcon($da['type']);
             // Calculate an appropriate unit size
-            $this->bean->size = context()->getFileSize($da['size']);
+            $this->bean->size = $context->getFileSize($da['size']);
         }
 /**
  * Called when you try to trash to an upload. Do any cleanup in here
