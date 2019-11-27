acb07fe0caa1a6e6c24627bbc4c405b9878065be
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-27 13:27:59 +0000

    Added content to home page.

--- class/pages/home.php
+++ class/pages/home.php
@@ -6,7 +6,7 @@
   * @copyright 2012-2019 Newcastle University
   */
     namespace Pages;
-    
+
     use \Support\Context as Context;
 /**
  * A class that contains code to implement a home page
@@ -22,6 +22,17 @@
  */
         public function handle(Context $context)
         {
+            $user = $context->user();
+            $uid = $user->id;
+            $context->local()->addval('user', $user);
+
+            // Get 6 top notes
+            $top = R::findAll('upload', 'JOIN note N ON N.id = upload.note_id
+                JOIN review R ON R.note_id = N.id WHERE R.rating > 0
+                GROUP BY N.id ORDER BY (SUM(R.rating) / COUNT(R.rating)) DESC,
+                N.upload DESC LIMIT 6');
+            $context->local()->addval('top', $context->canAccessFiles($top));
+
             return '@content/index.twig';
         }
     }
