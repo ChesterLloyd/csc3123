64a69c6a59a422da980771402c770490d9e3be2e
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-11-27 13:41:28 +0000

    Added login check to home page.

--- class/pages/home.php
+++ class/pages/home.php
@@ -23,16 +23,18 @@
  */
         public function handle(Context $context)
         {
-            $user = $context->user();
-            $uid = $user->id;
-            $context->local()->addval('user', $user);
-
-            // Get 6 top notes
-            $top = R::findAll('upload', 'JOIN note N ON N.id = upload.note_id
+            if ($context->hasuser())
+            { # User has logged in, show them the top 6 notes
+                $top = R::findAll('upload', 'JOIN note N ON N.id = upload.note_id
                 JOIN review R ON R.note_id = N.id WHERE R.rating > 0
                 GROUP BY N.id ORDER BY (SUM(R.rating) / COUNT(R.rating)) DESC,
                 N.upload DESC LIMIT 6');
-            $context->local()->addval('top', $context->canAccessFiles($top));
+                $context->local()->addval('top', $context->canAccessFiles($top));
+            }
+            else
+            {
+                $context->local()->addval('guest', true);
+            }
 
             return '@content/index.twig';
         }
