df8216d4851f023579ff07b884a5e5c580920713
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2019-12-05 11:54:37 +0000

    Added content to search.

--- class/pages/search.php
+++ class/pages/search.php
@@ -20,6 +20,24 @@
  */
         public function handle(Context $context)
         {
+            // Get search parameter from REST
+            $rest = $context->rest();
+            if (sizeof($rest) != 1)
+            { # No page or parameter passed
+                throw new \Framework\Exception\Forbidden('No access');
+            }
+            $param = filter_var('%'.$rest[0].'%', FILTER_SANITIZE_STRING);
+
+            $user = $context->user();
+            $uid = $user->id;
+            $context->local()->addval('user', $user);
+
+            // Get 6 top notes
+            $notes = R::findAll('upload', 'JOIN note N ON N.id = upload.note_id
+                WHERE N.name LIKE ? OR N.course LIKE ? OR N.module LIKE ?
+                GROUP BY N.id ORDER BY added DESC', [$param, $param, $param]);
+            $context->local()->addval('notes', $context->canAccessFiles($notes));
+
             return '@content/search.twig';
         }
     }
